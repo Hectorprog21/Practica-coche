18657ac79ce878c00baa9978f234c45a1ae74978
Hector <caminitodejerez@hotmail.com>
2023-03-20 17:32:39 +0100

    Practica coche

--- Test/Coche.java
+++ Test/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar_hector(int aceleracion) {
+    public void acelerar_Hector(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void decelerar_hector(int deceleracion) {
+    public void decelerar_Hector(int deceleracion) {
         velocidad -= deceleracion;
         if(velocidad<0) velocidad = 0;
     }
--- Test/TestCoche.java
+++ Test/TestCoche.java
@@ -3,28 +3,28 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_al_crear_un_coche_su_velocidad_es_cero(){
+    public void test_al_crear_un_coche_su_velocidad_es_cero_Hector(){
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0,nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_acelerar_un_coche_su_velocidad_es_aumenta(){
+    public void test_al_acelerar_un_coche_su_velocidad_se_aumenta_Hector(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar_hector(30);
+        nuevoCoche.acelerar_Hector(30);
         Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_es_disminuye(){
+    public void test_al_decelerar_un_coche_su_velocidad_se_disminuye_Hector(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar_hector(20);
+        nuevoCoche.decelerar_Hector(20);
         Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_de_cero(){
+    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_de_cero_Hector(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar_hector(80);
+        nuevoCoche.decelerar_Hector(80);
         Assertions.assertEquals(0,nuevoCoche.velocidad);
     }
 
